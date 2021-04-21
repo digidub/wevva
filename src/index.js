09092bf8012af31b61f5eb6a571a8115c1caeaa0
Sholto <madein.mania@gmail.com>
2021-04-21 09:39:53 +0100

    tie in weather to object

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import getWeather from './appcalls';
 import domcontrol from './domcontrol';
+import weatherToObject from './weatherobject';
 
-domcontrol.submitBtn.onclick = function (e) {
+domcontrol.submitBtn.onclick = async function (e) {
   e.preventDefault();
   const searchTerm = domcontrol.searchTerm.value;
-  getWeather(searchTerm);
+  const result = await getWeather(searchTerm);
+  console.log(weatherToObject(result));
 };
