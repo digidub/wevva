0fb02e63fb7bf3a3711bd1acc224d5d2c91224fd
Sholto <madein.mania@gmail.com>
2021-04-23 14:37:24 +0100

    basic object to dom function

--- src/domcontrol.js
+++ src/domcontrol.js
@@ -2,8 +2,17 @@ const domcontrol = (() => {
   // DOM identifiers
   const searchTerm = document.querySelector('.search');
   const submitBtn = document.querySelector('.submit');
+  const cityName = document.querySelector('.city-name');
+  const weatherIconDiv = document.querySelector('.weather-icon');
 
-  return { searchTerm, submitBtn };
+  const displayData = (obj) => {
+    cityName.innerText = `${obj.city}, ${obj.country}`;
+    const weatherIcon = new Image();
+    weatherIcon.src = `http://openweathermap.org/img/wn/${obj.icon}@2x.png`;
+    weatherIconDiv.appendChild(weatherIcon);
+  };
+
+  return { searchTerm, submitBtn, displayData };
 })();
 
 export default domcontrol;
