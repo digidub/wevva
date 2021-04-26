85d029233f3f4744350071642b664f3a7d2f6d3c
Sholto <madein.mania@gmail.com>
2021-04-26 10:07:07 +0100

    refactored object data extraction template to work with multiple API returns

--- src/weatherobject.js
+++ src/weatherobject.js
@@ -1,12 +1,20 @@
-function weatherToObject(object) {
-  return {
-    city: object.name,
-    country: object.sys.country,
-    time: object.dt,
-    temp: object.main.temp,
-    icon: object.weather[0].icon,
-    desc: object.weather[0].description,
-  };
-}
+const ObjectTemplates = (() => {
+  function dataExtractor(data) {
+    const obj = {};
+    if (data.name) obj.city = data.name;
+    if (data.sys?.country) obj.country = data.sys.country;
+    if (data.dt) obj.time = data.dt;
+    if (data.main?.temp) obj.temp = data.main.temp;
+    if (data.temp?.max) obj.max = data.temp.max;
+    if (data.temp?.min) obj.min = data.temp.min;
+    if (data.weather[0].icon) obj.icon = data.weather[0].icon;
+    if (data.weather[0].description) obj.desc = data.weather[0].description;
+    return obj;
+  }
 
-export default weatherToObject;
+  return {
+    dataExtractor,
+  };
+})();
+
+export default ObjectTemplates;
