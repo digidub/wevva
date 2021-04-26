b66909c873545ba3f36985c182d6b8e60d3d1c49
Sholto <madein.mania@gmail.com>
2021-04-26 10:09:03 +0100

    added function for calling extra API for additional weather info

--- src/appcalls.js
+++ src/appcalls.js
@@ -1,21 +1,47 @@
 // eslint-disable-next-line consistent-return
-function getWeather(search) {
-  return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a0f91643f488572be003cc868721e65a`)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      if (response.status === 404) {
-        return Promise.reject(new Error('error 404, city not found'));
-      }
-      return Promise.reject(new Error(response.status));
-    })
-    .then((response) => {
-      console.log(response);
-      return response;
-    })
-    .catch((error) => console.log(error));
-}
+
+const ApiCalls = (() => {
+  function getWeather(search) {
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a0f91643f488572be003cc868721e65a`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        if (response.status === 404) {
+          return Promise.reject(new Error('error 404, city not found'));
+        }
+        return Promise.reject(new Error(response.status));
+      })
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .catch((error) => console.log(error));
+  }
+
+  function getWeatherForecast(lat, lon) {
+    return fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=a0f91643f488572be003cc868721e65a`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        if (response.status === 404) {
+          return Promise.reject(new Error('error 404, lat/lon not found'));
+        }
+        return Promise.reject(new Error(response.status));
+      })
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .catch((error) => console.log(error));
+  }
+
+  return {
+    getWeather,
+    getWeatherForecast,
+  };
+})();
 
 // async function getWeatherMap(x, y) {
 //   const response = await fetch(`http://api.openweathermap.org/maps/2.0/weather/TA2/1/${x}/${y}&appid=a0f91643f488572be003cc868721e65a`);
@@ -41,4 +67,4 @@ function getWeather(search) {
 //     console.log(e);
 //   }
 
-export default getWeather;
+export default ApiCalls;
