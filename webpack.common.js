b6049390c04b1f33a202b807235071902e88b2f2
Sholto <madein.mania@gmail.com>
2021-04-27 14:17:11 +0100

    webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -2,37 +2,36 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.resolve(__dirname, "src", "index.html"),
-            inject: false,
-        }),
-        new CleanWebpackPlugin({ protectWebpackAssets: false, }),
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      inject: false,
+    }),
+    new CleanWebpackPlugin({ protectWebpackAssets: false }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                enforce: "pre",
-                use: ["source-map-loader"],
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,15 @@
 const { merge } = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        port: 9000
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 9000,
+    watchOptions: {
+      ignored: path.resolve(__dirname, 'node_modules'),
     },
+  },
 });
