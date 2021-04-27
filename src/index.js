60b645a0523d10744097a9bc7b285061c0a1f27f
Sholto <madein.mania@gmail.com>
2021-04-27 11:27:56 +0100

    map display on page load

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import domcontrol from './domcontrol';
 import ApiCalls from './appcalls';
 import ObjectTemplates from './weatherobject';
 
+window.onload = () => {
+  ApiCalls.initiateMap(0, 0);
+};
+
 domcontrol.submitBtn.onclick = function searchButton(e) {
   e.preventDefault();
   const searchTerm = domcontrol.searchTerm.value;
+  if (searchTerm === '') {
+    domcontrol.error();
+    return;
+  }
   domcontrol.loader();
   function getResults(search) {
     return ApiCalls.getWeather(search);
@@ -13,7 +21,8 @@ domcontrol.submitBtn.onclick = function searchButton(e) {
   getResults(searchTerm)
     .then((result) => {
       const weatherObj = ObjectTemplates.dataExtractor(result);
-      domcontrol.displayData(weatherObj);
+      domcontrol.displayCity(weatherObj);
+      ApiCalls.initiateMap(result.coord.lat, result.coord.lon);
       return ApiCalls.getWeatherForecast(result.coord.lat, result.coord.lon);
     })
     .then((result) => {
