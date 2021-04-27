ceec44be5cb77d5d7c5311481f7ac7f20df7a039
Sholto <madein.mania@gmail.com>
2021-04-27 09:03:42 +0100

    openweather openlayers map integration

--- src/appcalls.js
+++ src/appcalls.js
@@ -1,4 +1,10 @@
 // eslint-disable-next-line consistent-return
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+import XYZ from 'ol/source/XYZ';
 
 const ApiCalls = (() => {
   function getWeather(search) {
@@ -37,9 +43,54 @@ const ApiCalls = (() => {
       .catch((error) => console.log(error));
   }
 
+  function getWeatherMap(lat, lon) {
+    return fetch(`http://api.openweathermap.org/maps/2.0/weather/TA2/1/${lat}/${lon}&appid=a0f91643f488572be003cc868721e65a`, {
+      mode: 'no-cors',
+      credentials: 'include',
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        if (response.status === 404) {
+          return Promise.reject(new Error('error 404, lat/lon not found'));
+        }
+        return Promise.reject(new Error(response.status));
+      })
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .catch((error) => console.log(error));
+  }
+
+  function initiateMap(lat, lon) {
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=a0f91643f488572be003cc868721e65a',
+          }),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([lon, lat]),
+        zoom: 8,
+      }),
+    });
+    console.log(map);
+    // map.render();
+  }
+
   return {
     getWeather,
     getWeatherForecast,
+    getWeatherMap,
+    initiateMap,
   };
 })();
 
